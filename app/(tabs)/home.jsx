0107b1ddd52ebb53a2501720c3cb6bd06b7d89a6
faridfrs19 <faridgans129@gmail.com>
2024-11-06 12:52:18 +0700

    menghubungkan ke firebase, dan membuat header di home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,24 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '../../components/Home/Header'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{
+        padding:20,
+        marginTop:20
+    }}>
+      
+        {/* Header */}
+        <Header />
+        {/* Slider */}
+
+        {/* Category */}
+
+        {/* List app */}
+
+        {/* Add new app */}
+
     </View>
   )
 }
---
+++ config/FirebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "react-native-expo-e4a4e.firebaseapp.com",
+  projectId: "react-native-expo-e4a4e",
+  storageBucket: "react-native-expo-e4a4e.firebasestorage.app",
+  messagingSenderId: "229792852709",
+  appId: "1:229792852709:web:2fb783b6b0c9a5ef64c20c",
+  measurementId: "G-Z8C6RJX3GH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
