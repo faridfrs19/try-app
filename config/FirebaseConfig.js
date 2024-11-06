62a81bec927309c755a0e66d23a13c93295dd517
faridfrs19 <faridgans129@gmail.com>
2024-11-06 13:04:08 +0700

    menambahkan db firebase

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -18,4 +18,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db=getFirestore(app)
+// const analytics = getAnalytics(app);
