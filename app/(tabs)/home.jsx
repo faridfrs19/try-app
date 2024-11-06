e9c2c3ee77b1db8a6e70df1532a2ad63f3574070
faridfrs19 <faridgans129@gmail.com>
2024-11-06 14:05:59 +0700

    menambahkan component slider

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
+import Slider from '../../components/Home/Slider'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         {/* Header */}
         <Header />
         {/* Slider */}
+        <Slider />
 
         {/* Category */}
 
